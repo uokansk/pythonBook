f4cf2b758327c56733614040dad98e4293f47065
Aleksandr <uorkansk@yandex.ru>
2024-01-05 20:49:23 +0700

    задачи 2 главы

--- chapter1/bottles _beer.py
+++ chapter1/bottles _beer.py
@@ -1,21 +1,21 @@
-world = "бутылок"
+word = "бутылок"
 for beer_num in range(99, 0, -1):
     if beer_num == 1:
-        world = "бутылка"
-    print(beer_num, world, "пива на стене")
-    print(beer_num, world)
+        word = "бутылка"
+    print(beer_num, word, "пива на стене")
+    print(beer_num, word)
     print("возьми одну")
     print("пусти по кругу")
     if beer_num == 1:
         print("нет больше пива на стене")
     else:
         if beer_num >= 11 and beer_num <= 19:
-            world = "бутылок"
+            word = "бутылок"
         else:
             if beer_num % 10 == 1:
-                world = "бутылка"
+                word = "бутылка"
             elif beer_num % 10 in (2, 3, 4):
-                world = "бутылки"
+                word = "бутылки"
             else:
-                world = "бутылок"
+                word = "бутылок"
     print()
---
+++ chapter2/panic.py
@@ -0,0 +1,16 @@
+# on tap
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.pop(2)
+plist.insert(2, plist.pop(3))
+plist.insert(4, plist.pop())
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
